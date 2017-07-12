1132e34b43f43bf39436eaac3799d8158ce011cf
Juan José Rendón Yepes <juan.rendon@yuxiglobal.com>
2017-07-12 08:24:16 -0500

    homework 1 done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'My Application';
+  author = "Juan Rendon";
+  count = 0;
+  imgSource = "./../favicon.ico";
+  enable = true;
 }
