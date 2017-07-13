20e3305a843073e627f52a75f2c2c27550534a6b
Juan Rendon <juan.rendon@yuxiglobal.com>
2017-07-13 15:10:19 -0500

    build: Image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,6 @@ export class AppComponent {
   title = 'My Application';
   author = "Juan Rendon";
   count = 0;
-  imgSource = "./favicon.ico";
+  imgSource = "http://vectorlogo4u.com/wp-content/uploads/2016/05/angular-icon-vector.png";
   enable = true;
-}
+}   
